2e49b78e43b8a14d0a46c215662b5e9dec53af61
Siarhei Fedarenka <sergeifedorenkon@gmail.com>
2025-10-06 22:22:29 +0200

    Add service worker activation event to manage cache cleanup and force client updates

--- web/sw.js
+++ web/sw.js
@@ -28,3 +28,21 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      // Force update of all clients
+      return self.clients.claim();
+    })
+  );
+});
